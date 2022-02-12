7dc17d8aefbc6229cca1c95503a811f70ca4bab0
mikechirkov44 <90888317+mikechirkov44@users.noreply.github.com>
2022-02-12 21:32:00 +0300

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -12,11 +12,11 @@ SELECT
 	o.plan_delivery_date
 FROM orders o
 JOIN nomenclature n ON n.id = o.nomenclature_id
-WHERE n.name = 'Ткань';
+WHERE n.name = 'РўРєР°РЅСЊ';
 
 SELECT * FROM vw_ord_and_nmc;
 
--- считаем кол-во ожидаемых заказов по месяцам
+-- СЃС‡РёС‚Р°РµРј РєРѕР»-РІРѕ РѕР¶РёРґР°РµРјС‹С… Р·Р°РєР°Р·РѕРІ РїРѕ РјРµСЃСЏС†Р°Рј
 
 SELECT 
 	MONTHNAME (plan_delivery_date) AS month_name, 
@@ -25,13 +25,13 @@ FROM orders
 GROUP BY month_name 
 ORDER BY cnt DESC;
 
--- считаем среднюю стоимость товарной позиции в заказе и записываем ее в переменную.
--- далее выберем товарные позиции, цена которых выше средней по всем заказам.
+-- СЃС‡РёС‚Р°РµРј СЃСЂРµРґРЅСЋСЋ СЃС‚РѕРёРјРѕСЃС‚СЊ С‚РѕРІР°СЂРЅРѕР№ РїРѕР·РёС†РёРё РІ Р·Р°РєР°Р·Рµ Рё Р·Р°РїРёСЃС‹РІР°РµРј РµРµ РІ РїРµСЂРµРјРµРЅРЅСѓСЋ.
+-- РґР°Р»РµРµ РІС‹Р±РµСЂРµРј С‚РѕРІР°СЂРЅС‹Рµ РїРѕР·РёС†РёРё, С†РµРЅР° РєРѕС‚РѕСЂС‹С… РІС‹С€Рµ СЃСЂРµРґРЅРµР№ РїРѕ РІСЃРµРј Р·Р°РєР°Р·Р°Рј.
 
 SELECT AVG(unit_price) INTO @avg_price FROM orders;
 SELECT * FROM orders WHERE unit_price > @avg_price; 
 
--- посмотреть все заявки в отдел снабжения из основого производства (id = 2)
+-- РїРѕСЃРјРѕС‚СЂРµС‚СЊ РІСЃРµ Р·Р°СЏРІРєРё РІ РѕС‚РґРµР» СЃРЅР°Р±Р¶РµРЅРёСЏ РёР· РѕСЃРЅРѕРІРѕРіРѕ РїСЂРѕРёР·РІРѕРґСЃС‚РІР° (id = 2)
 
 SELECT 
 	r.id,
@@ -45,31 +45,31 @@ JOIN requests r ON d.id = r.department_id
 JOIN nomenclature n ON r.nomenclature_id = n.id
 WHERE d.id = 2;
 
--- Посчитаем кол-во поставщиков по странам
+-- РџРѕСЃС‡РёС‚Р°РµРј РєРѕР»-РІРѕ РїРѕСЃС‚Р°РІС‰РёРєРѕРІ РїРѕ СЃС‚СЂР°РЅР°Рј
 SELECT 
-	COUNT(*) AS 'Кол-во поставщиков',
-	country AS 'Страна'
+	COUNT(*) AS 'РљРѕР»-РІРѕ РїРѕСЃС‚Р°РІС‰РёРєРѕРІ',
+	country AS 'РЎС‚СЂР°РЅР°'
 FROM suppliers 
 GROUP BY country 
 ORDER BY id DESC;
 
--- Самый "опытный" менеджер:
+-- РЎР°РјС‹Р№ "РѕРїС‹С‚РЅС‹Р№" РјРµРЅРµРґР¶РµСЂ:
 
 SELECT id, firstname, lastname, started_at FROM managers ORDER BY started_at LIMIT 1;
 
--- Стаж работы менеджеров отделов снабжения и аутсорсинга в годах
+-- РЎС‚Р°Р¶ СЂР°Р±РѕС‚С‹ РјРµРЅРµРґР¶РµСЂРѕРІ РѕС‚РґРµР»РѕРІ СЃРЅР°Р±Р¶РµРЅРёСЏ Рё Р°СѓС‚СЃРѕСЂСЃРёРЅРіР° РІ РіРѕРґР°С…
 
-SELECT firstname, lastname, `position`, TIMESTAMPDIFF(YEAR, started_at, NOW()) AS 'Стаж' FROM managers;
+SELECT firstname, lastname, `position`, TIMESTAMPDIFF(YEAR, started_at, NOW()) AS 'РЎС‚Р°Р¶' FROM managers;
 
--- Функция для расчета активности отделов (процент заявок в отдел снабжения)
+-- Р¤СѓРЅРєС†РёСЏ РґР»СЏ СЂР°СЃС‡РµС‚Р° Р°РєС‚РёРІРЅРѕСЃС‚Рё РѕС‚РґРµР»РѕРІ (РїСЂРѕС†РµРЅС‚ Р·Р°СЏРІРѕРє РІ РѕС‚РґРµР» СЃРЅР°Р±Р¶РµРЅРёСЏ)
 
 DROP FUNCTION IF EXISTS suppliers_base.dep_activity;
 
 CREATE FUNCTION suppliers_base.dep_activity(dep_id BIGINT)
 RETURNS FLOAT READS SQL DATA 
 BEGIN
-	DECLARE requests_from_dep INT; -- кол-во заявок в отдел снабжения
-	DECLARE total_requests_qty INT; -- общее кол-во заявок
+	DECLARE requests_from_dep INT; -- РєРѕР»-РІРѕ Р·Р°СЏРІРѕРє РІ РѕС‚РґРµР» СЃРЅР°Р±Р¶РµРЅРёСЏ
+	DECLARE total_requests_qty INT; -- РѕР±С‰РµРµ РєРѕР»-РІРѕ Р·Р°СЏРІРѕРє
 
 	SET requests_from_dep = (
 		SELECT count(*) 
@@ -83,14 +83,14 @@ BEGIN
 	RETURN truncate((requests_from_dep / total_requests_qty),2) * 100;
 END
 
--- Посчитаем % заявок в отдел снабжения от Отдела продаж (id = 6)
-SELECT suppliers_base.dep_activity(6) AS '% заявок в отдел снабжения';
+-- РџРѕСЃС‡РёС‚Р°РµРј % Р·Р°СЏРІРѕРє РІ РѕС‚РґРµР» СЃРЅР°Р±Р¶РµРЅРёСЏ РѕС‚ РћС‚РґРµР»Р° РїСЂРѕРґР°Р¶ (id = 6)
+SELECT suppliers_base.dep_activity(6) AS '% Р·Р°СЏРІРѕРє РІ РѕС‚РґРµР» СЃРЅР°Р±Р¶РµРЅРёСЏ';
 
--- Вывести ТОП-3 контрагента по кол-ву заказов
+-- Р’С‹РІРµСЃС‚Рё РўРћРџ-3 РєРѕРЅС‚СЂР°РіРµРЅС‚Р° РїРѕ РєРѕР»-РІСѓ Р·Р°РєР°Р·РѕРІ
 CREATE OR REPLACE VIEW vw_top_suppliers AS
 SELECT
 	supplier_id,
-	(SELECT company_name FROM suppliers WHERE id = supplier_id) AS 'Контрагент',
+	(SELECT company_name FROM suppliers WHERE id = supplier_id) AS 'РљРѕРЅС‚СЂР°РіРµРЅС‚',
 	COUNT(*) AS cnt
 FROM orders
 GROUP BY supplier_id
@@ -98,8 +98,8 @@ ORDER BY cnt DESC;
 
 SELECT * FROM vw_top_suppliers LIMIT 3;
 
--- Процедура для осуществления платежей при помощи транзакции с ветвлением.
--- В качестве аргументов передаем сумму платежа и ID заказа. 
+-- РџСЂРѕС†РµРґСѓСЂР° РґР»СЏ РѕСЃСѓС‰РµСЃС‚РІР»РµРЅРёСЏ РїР»Р°С‚РµР¶РµР№ РїСЂРё РїРѕРјРѕС‰Рё С‚СЂР°РЅР·Р°РєС†РёРё СЃ РІРµС‚РІР»РµРЅРёРµРј.
+-- Р’ РєР°С‡РµСЃС‚РІРµ Р°СЂРіСѓРјРµРЅС‚РѕРІ РїРµСЂРµРґР°РµРј СЃСѓРјРјСѓ РїР»Р°С‚РµР¶Р° Рё ID Р·Р°РєР°Р·Р°. 
 
 DROP PROCEDURE IF EXISTS suppliers_base.arrange_payment;
 CREATE PROCEDURE suppliers_base.arrange_payment(IN amount FLOAT, IN id BIGINT)
@@ -113,18 +113,18 @@ BEGIN
 			SELECT balance, paid_amount FROM payments WHERE order_id = id;
 			COMMIT;
 		WHEN amount < 0 THEN 
-		 	SELECT 'Вы пытаетесь совершить платеж с отрицательной суммой!';
+		 	SELECT 'Р’С‹ РїС‹С‚Р°РµС‚РµСЃСЊ СЃРѕРІРµСЂС€РёС‚СЊ РїР»Р°С‚РµР¶ СЃ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕР№ СЃСѓРјРјРѕР№!';
 		ELSE 
-			SELECT 'Сумма платежа превышает остаток по задолженности!';
+			SELECT 'РЎСѓРјРјР° РїР»Р°С‚РµР¶Р° РїСЂРµРІС‹С€Р°РµС‚ РѕСЃС‚Р°С‚РѕРє РїРѕ Р·Р°РґРѕР»Р¶РµРЅРЅРѕСЃС‚Рё!';
 	END CASE;
 END
 
--- Проверим работу процедуры arrange_payment:
+-- РџСЂРѕРІРµСЂРёРј СЂР°Р±РѕС‚Сѓ РїСЂРѕС†РµРґСѓСЂС‹ arrange_payment:
 
 CALL arrange_payment (2000, 1);
 CALL arrange_payment (3040404, 1);
 
--- Процедура для размещения заявки в отдел снабжения.
+-- РџСЂРѕС†РµРґСѓСЂР° РґР»СЏ СЂР°Р·РјРµС‰РµРЅРёСЏ Р·Р°СЏРІРєРё РІ РѕС‚РґРµР» СЃРЅР°Р±Р¶РµРЅРёСЏ.
 
 DROP PROCEDURE IF EXISTS suppliers_base.send_request;
 CREATE PROCEDURE suppliers_base.send_request (IN nom_id BIGINT, IN dep_id BIGINT, IN qty INT)
@@ -136,10 +136,10 @@ BEGIN
 		SELECT * FROM requests ORDER BY id  DESC LIMIT 1;
 END
 
--- Проверим работы процедуры
+-- РџСЂРѕРІРµСЂРёРј СЂР°Р±РѕС‚С‹ РїСЂРѕС†РµРґСѓСЂС‹
 CALL send_request (5, 3, 350);
 
--- Триггер - запрет на размещение заказов у поставщиков со статусом "Blocked"
+-- РўСЂРёРіРіРµСЂ - Р·Р°РїСЂРµС‚ РЅР° СЂР°Р·РјРµС‰РµРЅРёРµ Р·Р°РєР°Р·РѕРІ Сѓ РїРѕСЃС‚Р°РІС‰РёРєРѕРІ СЃРѕ СЃС‚Р°С‚СѓСЃРѕРј "Blocked"
 
 DROP TRIGGER IF EXISTS check_if_supplier_blocked;
 CREATE TRIGGER check_if_supplier_blocked AFTER INSERT ON orders
@@ -148,11 +148,11 @@ BEGIN
 		SELECT `status` INTO @supplier_status FROM suppliers 
 		WHERE id = (SELECT supplier_id FROM orders WHERE id = NEW.id); 
 		IF(@supplier_status = 'blocked') THEN
-		SIGNAL SQLSTATE '45000' SET message_text = 'Размещение заказов у поставщиков со статусом "Blocked" запрещено';
+		SIGNAL SQLSTATE '45000' SET message_text = 'Р Р°Р·РјРµС‰РµРЅРёРµ Р·Р°РєР°Р·РѕРІ Сѓ РїРѕСЃС‚Р°РІС‰РёРєРѕРІ СЃРѕ СЃС‚Р°С‚СѓСЃРѕРј "Blocked" Р·Р°РїСЂРµС‰РµРЅРѕ';
      	END IF;
 END
 
--- Функция для подсчета % англоговорящих менеджеров
+-- Р¤СѓРЅРєС†РёСЏ РґР»СЏ РїРѕРґСЃС‡РµС‚Р° % Р°РЅРіР»РѕРіРѕРІРѕСЂСЏС‰РёС… РјРµРЅРµРґР¶РµСЂРѕРІ
 
 DROP FUNCTION IF EXISTS suppliers_base.english_speakers;
 CREATE FUNCTION suppliers_base.english_speakers()
@@ -162,6 +162,7 @@ BEGIN
 	SET @total_managers = (SELECT COUNT(*) FROM managers);
 	RETURN truncate((@speak_engl / @total_managers), 2) * 100;  
 END
--- проверим работу функции
+-- РїСЂРѕРІРµСЂРёРј СЂР°Р±РѕС‚Сѓ С„СѓРЅРєС†РёРё
 SELECT english_speakers();
+
 
